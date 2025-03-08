032587d3182a59c7d66c8c38b73191d751d9464a
Robert Morelli <robertondino@outlook.com>
2025-03-08 13:30:54 -0700

    no more weird pointer shit

--- pagetables.zig
+++ pagetables.zig
@@ -1,33 +1,29 @@
 const std = @import("std");
-
 const mmu = @import("mmu.zig");
 const pte_t = u32;
 const pde_t = u32;
 const pagedirPointers = [mmu.NPDENTRIES]*[mmu.PGSIZE]u8;
 const pagedir = union { pointers: pagedirPointers, table: [mmu.NPDENTRIES]pde_t };
-const pagetablePointers = [mmu.NPDENTRIES][*]u8;
-const pagetable = union { pointers: pagetablePointers, table: [mmu.NPDENTRIES]pte_t };
 
 var gdt: [mmu.NSEGS]mmu.segdesc = std.mem.zeroes([mmu.NSEGS]mmu.segdesc);
 var gdtdesc: gdtdesc = gdtdesc{ .limit = @sizeOf(gdt) - 1, .base = &gdt[0] };
 
-const trs: [*]allowzero u8 = @ptrFromInt(0);
-pub var entry_pgtable1: pagetable align(mmu.PGSIZE) = make_pte: {
-    var result: pagetablePointers = undefined;
+pub var entry_pgtable1: [mmu.NPDENTRIES]pte_t align(mmu.PGSIZE) = make_pte: {
+    var result: [mmu.NPDENTRIES]pte_t = undefined;
     @setEvalBranchQuota(mmu.NPTENTRIES + 1);
     for (0..mmu.NPTENTRIES) |i| {
-        result[i] = @as([*]u8, @ptrCast(&trs[((0x001000 * @as(u32, i)) | mmu.PTE_P | mmu.PTE_W)]));
+        result[i] = ((0x001000 * @as(u32, i)) | mmu.PTE_P | mmu.PTE_W);
     }
-    break :make_pte .{ .pointers = result };
+    break :make_pte result;
 };
 
-pub var entry_pgtable2: pagetable align(mmu.PGSIZE) = make_pte: {
-    var result: pagetablePointers = undefined;
+pub var entry_pgtable2: [mmu.NPDENTRIES]pte_t align(mmu.PGSIZE) = make_pte: {
+    var result: [mmu.NPDENTRIES]pte_t = undefined;
     @setEvalBranchQuota(mmu.NPTENTRIES + 1);
     for (0..mmu.NPTENTRIES) |i| {
-        result[i] = @as([*]u8, @ptrCast(&trs[(0x80000 | (0x001000 * @as(u32, i)) | mmu.PTE_P | mmu.PTE_W)]));
+        result[i] = (0x80000 | (0x001000 * @as(u32, i)) | mmu.PTE_P | mmu.PTE_W);
     }
-    break :make_pte .{ .pointers = result };
+    break :make_pte result;
 };
 
 pub var entry_pgdir: pagedir align(mmu.PGSIZE) = make_pdt: {
